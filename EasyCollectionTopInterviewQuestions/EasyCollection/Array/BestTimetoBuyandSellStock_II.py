0ddeb4d561da9e0049eebe527454d6fe2a630373
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-06-25 00:39:17 +0600

    BestTimetoBuyandSellStock_II update

--- EasyCollectionTopInterviewQuestions/EasyCollection/Array/BestTimetoBuyandSellStock_II.py
+++ EasyCollectionTopInterviewQuestions/EasyCollection/Array/BestTimetoBuyandSellStock_II.py
@@ -6,7 +6,7 @@ class Solution:
             return 0
         
         for i in range(len(prices)-1):
-            if prices[i+1] - prices[i] > 0:
+            if prices[i+1] > prices[i]:
                 profit+=(prices[i+1] - prices[i])
         
         return profit
